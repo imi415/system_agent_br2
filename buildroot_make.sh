2f851594c27815f3516270e2612010ce20ff065e
imi415 <imi415.public@gmail.com>
2021-06-20 17:57:42 +0800

    Add buildroot as submodule.

--- buildroot_make.sh
+++ buildroot_make.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ -z ${BUILDROOT_PATH} ]; then
-  echo "BUILDROOT_PATH environment variable is not set."
-  exit
-fi
-
-make O=${PWD}/output BR2_EXTERNAL=${PWD} -C ${BUILDROOT_PATH} "$@"
+make O=${PWD}/output BR2_EXTERNAL=${PWD}/external_tree -C buildroot "$@"
