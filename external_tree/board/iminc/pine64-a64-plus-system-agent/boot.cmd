51050c227b1b991f6fe131fa735b8336a451cfdf
imi415 <imi415.public@gmail.com>
2021-06-28 22:46:25 +0800

    Enable dtoverlay in boot.cmd.

--- external_tree/board/iminc/pine64-a64-plus-system-agent/boot.cmd
+++ external_tree/board/iminc/pine64-a64-plus-system-agent/boot.cmd
@@ -2,5 +2,10 @@ setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
 fatload mmc 0 $kernel_addr_r Image
 fatload mmc 0 $fdt_addr_r sun50i-a64-pine64-plus.dtb
+fatload mmc 0 0x4F000000 spidev.dtbo
+
+fdt addr ${fdt_addr_r}
+fdt resize 8192
+fdt apply 0x4F000000
 
 bootefi $kernel_addr_r $fdt_addr_r
