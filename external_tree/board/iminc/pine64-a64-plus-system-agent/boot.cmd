75c0d3832ccb20f92cd35e5a1b7dc6d7ae1a11a5
imi415 <imi415.public@gmail.com>
2021-06-29 02:15:12 +0800

    Fixed typos.

--- external_tree/board/iminc/pine64-a64-plus-system-agent/boot.cmd
+++ external_tree/board/iminc/pine64-a64-plus-system-agent/boot.cmd
@@ -2,7 +2,7 @@ setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
 fatload mmc 0 $kernel_addr_r Image
 fatload mmc 0 $fdt_addr_r sun50i-a64-pine64-plus.dtb
-fatload mmc 0 0x4F000000 spidev.dtbo
+fatload mmc 0 0x4F000000 overlays/spidev.dtbo
 
 fdt addr ${fdt_addr_r}
 fdt resize 8192
